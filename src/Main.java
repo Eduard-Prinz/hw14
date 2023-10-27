a31391d23dbecd5e3d95533ab55ebd3d7f34ddbd
Eduard-Prinz <edwardprince1112@gmail.com>
2023-10-27 14:27:39 +0300

    Выполнена заадача курсовой

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] array = {"apple", "banana", "orange", "apple", "banana"};
+        List<String> resultList = new ArrayList<>(new HashSet<>(Arrays.asList(array)));
+        String mergedString = String.join(" ", resultList);
+        System.out.println("Массив после удаления дубликатов: " + resultList);
     }
 }
